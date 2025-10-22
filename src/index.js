e2f4d1047e9c4a8ea45f3f3a2ecc7c170f49ad22
Akshata Sarawgi <akshata.sarawgi12@gmail.com>
2025-10-22 12:31:44 -0700

    Changes to index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-
 const generateIdea = () => {
-fetch('https://apis.scrimba.com/bored/api/activity')
-    .then(response => response.json())
-    .then(data => {
+fetch("https://apis.scrimba.com/bored/api/activity")
+.then(response => response.json())
+.then(data => {
     document.querySelector('.ideas').textContent = data.activity;
+    document.querySelector('.title').textContent = "HappyBot";
+    document.body.classList.add('fun')
 })
 }
 
-const button = document.querySelector('.button');
+const button = document.querySelector('.button')
 button.addEventListener('click', () => generateIdea())
